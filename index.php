3a6843a2c0e408a4a73a9b3628b9f07b3243a934
Leonardo Matos <leomp120894@gmail.com>
2018-11-18 00:55:13 -0200

    runing class with server params

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
 <?php
+require './lib/main.php';
+
+$products_feed = new ProductsFeed(
+  @$_SERVER['HTTP_X_STORE_ID'],
+  'https://' . $_SERVER['HTTP_X_STORE_DOMAIN'] . '/'
+);
+
+echo $products_feed->xml(
+  @$_GET['title'],
+  @$_GET['query_string'],
+  isset($_GET['set_properties']) ? json_decode($_GET['set_properties']) : null,
+  isset($_GET['product_ids']) ? json_decode($_GET['product_ids']) : null
+);
