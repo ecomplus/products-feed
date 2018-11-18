3a547a583ff3fe574e0c291a1936914882e560c8
Leonardo Matos <leomp120894@gmail.com>
2018-11-18 00:33:48 -0200

    handle body convertion to xml string

--- lib/main.php
+++ lib/main.php
@@ -1,9 +1,10 @@
 <?php
 class ProductsFeed {
   public $xml;
+  public $convert;
 
-  private $store_id;
-  private $base_uri;
+  private $store_id; // 1000
+  private $base_uri; // https://www.mysaleschannel.com/
   private $api_host;
 
   function __construct ($store_id, $base_uri, $api_host = 'https://api.e-com.plus/v1') {
@@ -51,12 +52,12 @@ class ProductsFeed {
     return $result;
   }
 
-  function xml ($title = 'Products feed', $promotion_id, $product_ids) {
+  function xml ($title = 'Products feed', $query_string, $set_properties, $product_ids) {
     $xml = <<<XML
 <?xml version="1.0"?>
 <feed xmlns="http://www.w3.org/2005/Atom" xmlns:g="http://base.google.com/ns/1.0">
   <title><![CDATA[$title]]></title>
-  <link rel="self" href="this->$base_uri"/>
+  <link rel="self" href="{this->$base_uri}"/>
   <updated>{date('Y-m-d\TH:i:s\Z')}</updated>
 XML;
 
@@ -64,8 +65,9 @@ XML;
       // get all products
       $json = $this->api_request('/products.json');
       $product_ids = [];
-      $products = json_decode($json);
+      $result = json_decode($json);
       if (json_last_error() === JSON_ERROR_NONE) {
+        $products = @$result->result;
         for ($i = 0; $i < count($products); $i++) {
           $product_ids[] = $products[$i]->_id;
         }
@@ -76,13 +78,16 @@ XML;
     for ($i = 0; $i < count($product_ids); $i++) {
       // delay to prevent 503 error
       usleep($i * 300);
-      $json = $this->api_request('/products' . $product_ids[$i] . '.json');
-      $product = json_decode($json);
+      $json = $this->api_request('/products/' . $product_ids[$i] . '.json');
+      $product = json_decode($json, true);
       if (json_last_error() === JSON_ERROR_NONE) {
-        // convert product to GMC XML entry
-        $xml += <<<XML
-  {$this->convert($product, $promotion_id)}
+        // check if product is available
+        if (@$product['available'] === true && @$product['visible'] === true) {
+          // convert product to GMC XML entry
+          $xml += <<<XML
+  {$this->convert($product, $query_string, $set_properties)}
 XML;
+        }
       }
     }
 
@@ -92,136 +97,216 @@ XML;
     return $xml;
   }
 
-  public function convert ($product, $promotion_id) {
-    $xml = '<entry>';
-    if (isset($product->_id)) {
-      $xml .= '<g:id>' . $product->_id . '<g:id>';
-    }
-    if (isset($product->name)) {
-      $xml .= '<g:title>' . $product->name . '<g:title>';
-    }
-    if (isset($product->body_text)) {
-      $xml .= '<g:description>' . $product->body_text . '<g:description>';
-    }
-    if (isset($product->slug)) {
-      $xml .= '<g:link>' . $dominio . '/' . $product->slug . '?' . $queryString . '<g:link>';
-    }
-    if (isset($product->pictures)) {
-      $xml .= '<g:image_link>' . $product->pictures[0]['zoom']  . '<g:image_link>';
-      $xml .= '<g:additional_image_link>' . $product->pictures[0]['zoom'] . '<g:additional_image_link>';
-    }
-    if (isset($product->mobile_link)) {
-      $xml .= '<g:mobile_link>' . $product->mobile_link . $queryString . '<g:mobile_link>';
-    }
-    if (isset($product->available)) {
-      $xml .= '<g:availability>' . $product->available == true ? 'in stock' : 'out of stock' . '<g:availability>';
-    }
-    //$xml .= '<g:availability_date>' . . '<g:availability_date>'; // optional
-    //$xml .= '<g:expiration_date>' . . '<g:expiration_date>'; // optional
+  function convert ($body, $query_string = '?_=feed', $set_properties, $group_id) {
+    if (isset($body['name']) && isset($body['_id'])) {
+      $entry = array(
+        'id' => isset($body['sku']) ? $body['sku'] : $body['_id'],
+        'title' => $body['name']
+      );
 
-    if (isset($product->price)) {
-      $xml .= '<g:price>' . $product->price . '<g:price>';
-    }
-    if (isset($product->other_price)) {
-      $xml .= '<g:sale_price>' . $product->other_price->price . '<g:sale_price>';
-    }
-    if (isset($product->price_change_records)) {
-      $xml .= '<g:sale_price_effective_date>' . $product->price_change_records->date_of_change . '<g:sale_price_effective_date>';
-    }
-
-    //$xml .= '<g:cost_of_goods_sold>' . . '<g:cost_of_goods_sold>'; // optional
-
-    if (isset($product->measurement)) {
-      $xml .= '<g:unit_pricing_measure>' . $product->measurement->unit . '<g:unit_pricing_measure>';
-      $xml .= '<g:unit_pricing_base_measure>' . $product->measurement->pricing_base_measure  . '<g:unit_pricing_base_measure>';
-    }
-    if (isset($product->installments)) {
-      $xml .= '<g:installment>' . '<g:months>'. $product->installments->number .'</g:months>'.'<g:amount>'. $product->installments->number .'</g:amount>'. '<g:installment>';
-    }
-    if (isset($product->google_product_category_id)) {
-      $xml .= '<g:google_product_category>' . $product->google_product_category_id . '<g:google_product_category>';
-    }
-    if (isset($product->categories)) {
-      $xml .= '<g:product_type>' . $this->getCategoriesParent($product->categories[0]->_id) . '<g:product_type>';
-    }
-    if (isset($product->brands)) {
-      $xml .= '<g:brand>' . $product->brands[0]->name. '<g:brand>';
-    }
-    if (isset($product->gtin)) {
-      $xml .= '<g:gtin>' . $product->gtin[0] . '<g:gtin>';
-    }
-    if (isset($product->mpn)) {
-      $xml .= '<g:mpn>' .$product->mpn[0] . '<g:mpn>';
-    }
-
-    $xml .= '<g:identifier_exists>' . !isset($product->brands[0]->name) && !isset($product->gtin[0]) && isset($product->mpn[0]) ? 'no' : 'yes' . '<g:identifier_exists>';
-
-    if (isset($product->condition)) {
-      $xml .= '<g:condition>' . $product->condition . '<g:condition>';
-    }
-    if (isset($product->adult)) {
-      $xml .= '<g:adult>' . $product->adult . '<g:adult>';
-    }
-    if (isset($product->multipack)) {
-      $xml .= '<g:multipack>' . $product->multipack . '<g:multipack>';
-    }
-
-    $xml .= '<g:is_bundle>' . 'no' . '<g:is_bundle>';
-
-    if (isset($product->specifications->energy_efficiency_class)) {
-      $xml .= '<g:energy_efficiency_class>' . $product->specifications->energy_efficiency_class->text . '<g:energy_efficiency_class>';  //optional
-      $xml .= '<g:min_energy_efficiency_class>' . $product->specifications->energy_efficiency_class->value. '<g:min_energy_efficiency_class>'; //optional
-      //$xml .= '<g:max_energy_efficiency_class>' . . '<g:max_energy_efficiency_class>'; //optional
-    }
-
-    if (isset($product->specifications)) {
-      $xml .= '<g:age_group>' . $product->specifications->age_group[0]->text . '<g:age_group>';
-      $xml .= '<g:color>' . $product->specifications->colors[0]->text . '<g:color>';
-      $xml .= '<g:gender>' . $product->specifications->gender[0]->value . '<g:gender>';
-      $xml .= '<g:material>' . $product->specifications->material[0]->value . '<g:material>';
-      $xml .= '<g:pattern>' . $product->specifications->pattern->value. '<g:pattern>';
-      $xml .= '<g:size>' . $product->specifications->size[0]->value . '<g:size>';
-      $xml .= '<g:size_type>' . $product->specifications->size_type[0]->value . '<g:size_type>';
-      $xml .= '<g:size_system>' . $product->specifications->size_system[0]->value. '<g:size_system>';
-    }
-
-    //$xml .= '<g:item_group_id>' . . '<g:item_group_id>'; //
-    foreach ($product->specifications as $key => $value) {
-      $xml .= "<g:custom_label_{$key}>" .$value. "<g:custom_label_{$key}>"; //optional
-    }
-
-    if ($promotion_id) {
-      $xml .= '<g:promotion_id>' . $promotion_id . '<g:promotion_id>'; //optional
-    }
-
-    if (isset($product->destinations)) {
-      $xml .= '<g:excluded_destination>' .$product->destinations->excluded->country . '<g:excluded_destination>'; // optional
-      $xml .= '<g:included_destination>' . $product->destinations->included->country . '<g:included_destination>'; // optional
-    }
-    if (isset($product->shipping_methods)) {
-      foreach ($product->shipping_methods as $value) {
-        $xml .= '<g:shipping>' . $value->label . '<g:shipping>';
-        $xml .= '<g:shipping_label>' .  $value->label . '<g:shipping_label>';
+      // text description
+      if (isset($body['body_text'])) {
+        $entry['description'] = $body['body_text'];
+      } else if (isset($body['body_html'])) {
+        $entry['description'] = strip_tags($body['body_html']);
       }
-    }
-    if (isset($product->weight)) {
-      $xml .= '<g:shipping_weight>' . $product->weight->value . $product->weight->unit . '<g:shipping_weight>';
-    }
-    if (isset($product->dimensions)) {
-      $xml .= '<g:shipping_length>' . $product->dimensions->length->value . '<g:shipping_length>';
-    }
-    if (isset($product->production_time)) {
-      $xml .= '<g:min_handling_time>' . $product->production_time->days . '<g:min_handling_time>'; //optional
-      $xml .= '<g:max_handling_time>' . $product->production_time->max_time . '<g:max_handling_time>'; //optional
-    }
 
-    //$xml .= '<g:tax>' . . '<g:tax>';
-    //$xml .= '<g:tax_category>' . . '<g:tax_category>';
+      // product page links
+      if (isset($body['permalink'])) {
+        $entry['link'] = $body['permalink'] . $queryString;
+      } else if (isset($body['slug'])) {
+        $entry['link'] = $this->base_uri . $body['slug'] . $queryString;
+      } else {
+        $entry['link'] = $this->base_uri . $queryString + '&_id=' . $body['_id'];
+      }
+      if (isset($body['mobile_link'])) {
+        $entry['mobile_link'] = $body['mobile_link'] . $queryString;
+      }
 
-    $xml .= '</entry>';
-    return $xml;
-  }
+      // stock
+      if (!isset($body['quantity']) || ($body['quantity'] && $body['quantity'] >= @$body['min_quantity'])) {
+        $entry['availability'] = 'in stock';
+      } else {
+        $entry['availability'] = 'out of stock';
+      }
 
-  public function getCategoriesParent ($id) {
+      // optional production (factory) time
+      if (@$body['production_time']['days']) {
+        $entry['min_handling_time'] = @$body['production_time']['days'];
+        if (@$body['production_time']['max_time']) {
+          $entry['max_handling_time'] = @$body['production_time']['max_time'];
+        }
+      }
+
+      // product photos
+      if (isset($body['pictures']) && count($body['pictures'])) {
+        $additional_images = '';
+        for ($i = 0; $i < count($body['pictures']) && $i < 10; $i++) {
+          $img = $body['pictures'][$i];
+          $img_link = null;
+          if (isset($img['zoom'])) {
+            $img_link = @$img['zoom']['url'];
+          } else if (isset($img['big'])) {
+            $img_link = @$img['big']['url'];
+          } else {
+            foreach ($img as $size => $img_obj) {
+              // get any image size excepting small
+              if ($size !== 'small') {
+                $img_link = @$img_obj['url'];
+                break;
+              }
+            }
+          }
+
+          if ($img_link) {
+            if ($i === 0) {
+              // first image
+              $entry['image_link'] = $img_link;
+            } else {
+              if ($additional_images !== '') {
+                $additional_images .= ',';
+              }
+              $additional_images .= str_replace(',', '%2C', $img_link);
+            }
+          }
+        }
+        if ($additional_images !== '') {
+          $entry['additional_image_link'] = $additional_images;
+        }
+      }
+
+      // prices
+      if (isset($body['price'])) {
+        if (isset($body['base_price'])) {
+          // promotional price
+          $entry['price'] = $body['base_price'] . ' ' . @$body['currency_id'];
+          $entry['sale_price'] = $body['price'] . ' ' . @$body['currency_id'];
+
+          if (isset($body['price_effective_date'])) {
+            if (isset($body['price_effective_date']['start'])) {
+              $date_range = $body['price_effective_date']['start'];
+            } else {
+              $date_range = date('Y-m-d\TH:i:s\Z');
+            }
+            if (isset($body['price_effective_date']['end'])) {
+              $date_range .= '/' . $body['price_effective_date']['start'];
+            } else {
+              // any future date
+              $date_range .= '/' . date('Y-m-d\TH:i:s\Z', strtotime('+60 days'));
+            }
+            $entry['sale_price_effective_date'] = $date_range;
+          }
+        } else {
+          // eg.: 10.00 BRL
+          $entry['price'] = $body['price'] . ' ' . @$body['currency_id'];
+        }
+      }
+
+      // categories
+      if (isset($body['google_product_category_id'])) {
+        $entry['google_product_category'] = $body['google_product_category_id'];
+      }
+      if (isset($body['categories'])) {
+        // @TODO
+      }
+
+      // brand name
+      $identifier_exists = false;
+      if (isset($body['brands']) && count($body['brands']) > 0) {
+        $entry['brand'] = @$body['brands'][0]['name'];
+        $identifier_exists = true;
+      }
+
+      // product codes
+      $codes = array('gtin', 'mpn');
+      for ($i = 0; $i < count($codes); $i++) {
+        $key = $codes[$i];
+        if (isset($body[$key]) && count($body[$key]) > 0) {
+          // send first code on array only
+          $entry[$key] = $body[$key][0];
+          if (!$identifier_exists) {
+            $identifier_exists = true;
+          }
+        }
+      }
+      // if brand and/or code(s)
+      $entry['identifier_exists'] = $identifier_exists ? 'yes' : 'no';
+
+      // product types
+      $entry['adult'] = @$body['adult'] === true ? 'yes' : 'no';
+      if (isset($body['condition'])) {
+        $entry['condition'] = $body['condition'] !== 'not_specified' ? $body['condition'] : 'new';
+      }
+
+      // product measures for shipping
+      if (isset($body['weight'])) {
+        $entry['shipping_weight'] = @$body['weight']['value'] . ' ' . @$body['weight']['unit'];
+      }
+      if (isset($body['dimensions'])) {
+        $dimensions = array('length', 'width', 'height');
+        for ($i = 0; $i < count($dimensions); $i++) {
+          $key = $dimensions[$i];
+          if ($measure_obj = @$body['dimensions'][$key]) {
+            $entry['shipping_' . $key] = @$measure_obj['value'] . ' ' . @$measure_obj['unit'];
+          }
+        }
+      }
+
+      // handling specs
+      $custom_label = 0;
+      foreach (@$body['specifications'] as $spec => $values) {
+        if (count($values) === 0) continue;
+        $value = $values[0];
+
+        switch ($spec) {
+          case 'energy_efficiency_class':
+          case 'age_group':
+          case 'gender':
+          case 'size_type':
+          case 'size_system':
+            $entry[$spec] = @$value['value'] ? $value['value'] : @$value['text'];
+            break;
+
+          case 'size':
+          case 'pattern':
+          case 'material':
+            $entry[$spec] = @$value['text'];
+            break;
+
+          case 'colors':
+            // send up to 3 colors
+            $colors = str_replace('/', ' ', @$value['text']);
+            for ($i = 1; $i < count($values); $i++) {
+              $colors .= '/' . str_replace('/', ' ', @$values[$i]['text']);
+            }
+            $entry[$spec] = $colors;
+            break;
+
+          default:
+            // send as custom label
+            if ($custom_label < 5) {
+              $entry['custom_label_' . $custom_label] = @$values[$i]['text'];
+            }
+            break;
+        }
+      }
+
+      if (is_array($set_properties)) {
+        // force some data properties
+        foreach ($set_properties as $key => $value) {
+          $entry[$key] = $value;
+        }
+      }
+
+      // parse to XML string
+      $xml = '<entry>';
+      foreach ($entry as $key => $value) {
+        $xml .= '<g:' . $key . '><![CDATA[' . $value . ']]></g:' . $key . '>';
+      }
+      return $xml . '</entry>';
+    } else {
+      // no product data
+      return '';
+    }
   }
 }
