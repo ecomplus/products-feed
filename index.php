06d8de621538db4f076cd10fc84c363d0be4a70d
Leonardo Matos <leomp120894@gmail.com>
2020-06-03 17:09:00 -0300

    fix(params): fix parsing json params (to assoc array)

--- index.php
+++ index.php
@@ -9,6 +9,6 @@ $products_feed = new ProductsFeed(
 echo $products_feed->xml(
   @$_GET['title'],
   @$_GET['query_string'],
-  isset($_GET['set_properties']) ? json_decode($_GET['set_properties']) : null,
-  isset($_GET['product_ids']) ? json_decode($_GET['product_ids']) : null
+  isset($_GET['set_properties']) ? json_decode($_GET['set_properties'], true) : null,
+  isset($_GET['product_ids']) ? json_decode($_GET['product_ids'], true) : null
 );
